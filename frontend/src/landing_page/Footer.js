907974cceab0238f56772116816a062aa5b374ec
tanmayeechanda <tanmayee_chanda@srmap.edu.in>
2026-07-02 01:50:08 +0530

    Landing page is successfully completed

--- frontend/src/landing_page/Footer.js
+++ frontend/src/landing_page/Footer.js
@@ -1,7 +1,116 @@
 import React from "react";
 
 function Footer() {
-  return <h1>Footer</h1>;
+  return (
+    <div className="container border-top mt-5">
+      <div className="row mt-5">
+        {" "}
+        <div className="col">
+          <img src="/media/images/logo.svg" alt="" style={{ width: "40%" }} />
+          <p>&copy;2010-2014 ,Not Zerodha Broking Ltd. All Rights Reserved.</p>
+        </div>
+        <div className="col">
+          <p>Company</p>
+          <a href="/">About</a>
+          <br />
+          <a href="/">Product</a>
+          <br />
+          <a href="/">Pricing</a>
+          <br />
+          <a href="/">Referral Program</a>
+          <br />
+          <a href="/">Careers</a>
+          <br />
+          <a href="/">Zerodha.tech</a>
+          <br />
+          <a href="/">Press and Media</a>
+          <br />
+          <a href="/">Zerodha cares</a>
+          <br />
+        </div>
+        <div className="col">
+          <p>Support</p>
+          <a href="/">Contact </a>
+          <br />
+          <a href="/">Support Portal</a>
+          <br />
+          <a href="/">Z-connect Blog</a>
+          <br />
+          <a href="/">List of Charges</a>
+          <br />
+          <a href="/">Downloads and resources</a>
+          <br />
+        </div>
+        <div className="col">
+          <p>Account</p>
+          <a href="/">Open an account</a>
+          <br />
+          <a href="/">Fund Transfer</a>
+          <br />
+          <a href="/">60 day challenge</a>
+          <br />
+        </div>
+      </div>
+      <div className="mt-5 text-muted " style={{ fontSize: "10.4x" }}>
+        <p>
+          Zerodha Broking Ltd.: Member of NSE, BSE, MCX & MSEI – SEBI
+          Registration no.: INZ000031633 CDSL/NSDL: Depository services through
+          Zerodha Broking Ltd. – SEBI Registration no.: IN-DP-431-2019
+          Registered Address: Zerodha Broking Ltd., #153/154, 4th Cross, Dollars
+          Colony, Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru -
+          560078, Karnataka, India. For any complaints pertaining to securities
+          broking please write to complaints@zerodha.com, for DP related to
+          dp@zerodha.com. Please ensure you carefully read the Risk Disclosure
+          Document as prescribed by SEBI | ICF
+        </p>
+        <p>
+          Procedure to file a complaint on SEBI SCORES: Register on SCORES
+          portal. Mandatory details for filing complaints on SCORES: Name, PAN,
+          Address, Mobile Number, E-mail ID. Benefits: Effective Communication,
+          Speedy redressal of the grievances Smart Online Dispute Resolution |
+          Grievances Redressal Mechanism Investments in securities market are
+          subject to market risks; read all the related documents carefully
+          before investing. Attention investors: 1) Stock brokers can accept
+          securities as margins from clients only by way of pledge in the
+          depository system w.e.f September 01, 2020. 2) Update your e-mail and
+          phone number with your stock broker / depository participant and
+          receive OTP directly from depository on your e-mail and/or mobile
+          number to create pledge. 3) Check your securities / MF / bonds in the
+          consolidated account statement issued by NSDL/CDSL every month.
+          India's largest broker based on networth as per NSE. NSE broker
+          factsheet
+        </p>
+        <p>
+          "Prevent unauthorised transactions in your account. Update your mobile
+          numbers/email IDs with your stock brokers/depository participants.
+          Receive information of your transactions directly from
+          Exchange/Depositories on your mobile/email at the end of the day.
+          Issued in the interest of investors. KYC is one time exercise while
+          dealing in securities markets - once KYC is done through a SEBI
+          registered intermediary (broker, DP, Mutual Fund etc.), you need not
+          undergo the same process again when you approach another
+          intermediary." Dear Investor, if you are subscribing to an IPO, there
+          is no need to issue a cheque. Please write the Bank account number and
+          sign the IPO application form to authorize your bank to make payment
+          in case of allotment. In case of non allotment the funds will remain
+          in your bank account. As a business we don't give stock tips, and have
+          not authorized anyone to trade on behalf of others. If you find anyone
+          claiming to be part of Zerodha and offering such services, please
+          create a ticket here.
+        </p>
+        <p>
+          Fixed deposit products offered on this platform are third-party
+          products (TPP) and are not Exchange traded products. These are offered
+          through Blostem Fintech Private Limited. Zerodha Broking Limited (SEBI
+          Registration No.: INZ000031633) is acting solely as a distributor for
+          these products. Any disputes arising with respect to such distribution
+          activity will not have access to SEBI SCORES/ODR, Exchange Investor
+          Grievance Redressal Forum, or Arbitration mechanism. Fixed deposits
+          are regulated by the Reserve Bank of India (RBI).
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
--- frontend/src/landing_page/Navbar.js
+++ frontend/src/landing_page/Navbar.js
@@ -1,7 +1,60 @@
 import React from "react";
 
 function Navbar() {
-  return <h1>Navbar</h1>;
+  return (
+    <nav
+      class="navbar navbar-expand-lg border-bottom "
+      style={{ backgroundColor: "#fff" }}
+    >
+      <div class="container">
+        <a class="navbar-brand" href="some">
+          <img src="/media/images/logo.svg" alt="" style={{ width: "25%" }} />
+        </a>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <form class="d-flex" role="search">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className="nav-link active" href="some">
+                  Signup
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="some">
+                  About
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="some">
+                  Product
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="some">
+                  Pricing
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" href="some">
+                  Support
+                </a>
+              </li>
+            </ul>
+          </form>
+        </div>
+      </div>
+    </nav>
+  );
 }
 
 export default Navbar;
